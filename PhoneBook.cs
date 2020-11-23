25a71833a8968b4059abd08294e6e507b488a727
Austin Schrader <austin.jschrader@gmail.com>
2020-11-23 14:02:25 -0800

    Add functionality for LookUpContact() method

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -42,6 +42,15 @@ class PhoneBook {
   }
 
   static void LookUpContact() {
-    // Nothing
+    Console.WriteLine("CONTACT LOOKUP");
+    Console.WriteLine("Whose number would you like to look up?");
+    string friend = Console.ReadLine();
+    if (phoneNumbers.ContainsKey(friend)) {
+      string value = phoneNumbers[friend];
+      Console.WriteLine(friend + "\'s phone number is " + value);
+    } else {
+      Console.WriteLine("That person is not in your phone book");
+    }
+    Main();
   }
 }
